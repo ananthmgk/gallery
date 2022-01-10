ee5edd7ff25e4e9cb92969bd846caea3a127203e
Ananth Murugan G K <ananthmgk4002@gmail.com>
2022-01-10 22:22:24 +0530

    Activated next prev buttons

--- main.js
+++ main.js
@@ -1,18 +1,55 @@
-const a = document.getElementById("image1");
-const b = document.getElementById("image2");
-const c = document.getElementById("image3");
+const image1 = "https://images.pexels.com/photos/459203/pexels-photo-459203.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
+const image2 = "https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
+const image3 = "https://images.pexels.com/photos/1519088/pexels-photo-1519088.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
+const image4 = "https://images.pexels.com/photos/1191710/pexels-photo-1191710.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
+const image5 = "https://images.pexels.com/photos/1532771/pexels-photo-1532771.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
 
-
-const displayer = document.querySelector('.show');
+const img = document.querySelector('img');
 const next = document.querySelector('.right-arrow');
 const previous = document.querySelector('.left-arrow');
-let display=" ";
+
+const imageList = [image1,image2,image3,image4,image5];
+
+/*
+*/
+let i = 0;
+let display = [i];
 next.addEventListener("click", ()=>{
-    display++;
-    displayer.innerHTML= display;
+    i++;
+    display = imageList[i];
+    img.src= display;
 });
 
 previous.addEventListener("click", ()=>{
-    display--;
-    displayer.innerHTML= display;
+    i--;
+    display = imageList[i];
+    img.src= display;
+});
+
+if(imageList[5]) {
+    
+}
+
+let btn1 = document.querySelector('#btn1');
+let btn2 = document.querySelector('#btn2');
+let btn3 = document.querySelector('#btn3');
+let btn4 = document.querySelector('#btn4');
+let btn5 = document.querySelector('#btn5');
+
+btn1.addEventListener('click', () => {
+    img.src = "https://images.pexels.com/photos/459203/pexels-photo-459203.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
+});
+
+btn2.addEventListener('click', () => {
+    img.src = "https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
+});
+
+btn3.addEventListener('click', () => {
+    img.src = "https://images.pexels.com/photos/1519088/pexels-photo-1519088.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
+});
+btn4.addEventListener('click', () => {
+    img.src = "https://images.pexels.com/photos/1191710/pexels-photo-1191710.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
+});
+btn5.addEventListener('click', () => {
+    img.src = "https://images.pexels.com/photos/1532771/pexels-photo-1532771.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
 });
