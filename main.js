5d5e2d89b228d482217dab103bdd55843ad05c23
Ananth Murugan G K <ananthmgk4002@gmail.com>
2022-01-16 15:32:29 +0530

    Activated zoom+- buttons

--- main.js
+++ main.js
@@ -1,60 +1,59 @@
-const image1 = "https://images.pexels.com/photos/459203/pexels-photo-459203.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
-const image2 = "https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
-const image3 = "https://images.pexels.com/photos/1519088/pexels-photo-1519088.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
-const image4 = "https://images.pexels.com/photos/1191710/pexels-photo-1191710.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
-const image5 = "https://images.pexels.com/photos/1532771/pexels-photo-1532771.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
-const imageList = [image1,image2,image3,image4,image5];
+const imageList = [
+    "https://images.pexels.com/photos/1532771/pexels-photo-1532771.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    "https://images.pexels.com/photos/459203/pexels-photo-459203.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    "https://images.pexels.com/photos/884788/pexels-photo-884788.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    "https://images.pexels.com/photos/1519088/pexels-photo-1519088.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    "https://images.pexels.com/photos/1191710/pexels-photo-1191710.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+];
 let i = 0;
-let display = imageList[0];
 
 document.addEventListener("DOMContentLoaded", function(){
-    document.querySelector('.right-arrow').addEventListener("click", ()=>{
-        if(display >= imageList[imageList.length]) {
-            next.classList.add('hideBtn');
-        } else if (display === imageList[4]) {
-            next.classList.remove('hideBtn');
-        } else {
-            i++;
-            display = imageList[i];
-            document.querySelector('img').src= display;
-        }
+    const nextBtn = document.getElementById('nextBtn');
+    const previousBtn = document.getElementById('previousBtn');
+    const img = document.getElementById('image');
+    
+    initialize()
+    
+    nextBtn.addEventListener("click", ()=>{
+        i++;
+        img.src= imageList[i];
+        navigationCheck();
     });
     
-    // previous
-    document.querySelector('.left-arrow').addEventListener("click", ()=>{
+  
+    previousBtn.addEventListener("click", ()=>{
         i--;
-        display = imageList[i];
-        document.querySelector('img').src= display;
-        if(display === imageList[0]) {
-            previous.classList.add('hideBtn');
-        } else if (display === imageList[1]) {
-            previous.classList.remove('hideBtn');
-        };
+        img.src= imageList[i];
+        navigationCheck();
+    });
+
+    zoomInBtn.addEventListener("click", ()=> {
+        document.getElementById("image").style.width = (document.getElementById("image").clientWidth + 100) + "px";
+        document.getElementById("image").style.height = (document.getElementById("image").clientWidth + 100) + "px";
     });
     
-    document.querySelector('#btn1').addEventListener('click', () => {
-        document.querySelector('img').src = image1;
-        i = 0;
+    zoomOutBtn.addEventListener("click", ()=> {
+        document.getElementById("image").style.width = (document.getElementById("image").clientWidth - 100) + "px";
+        document.getElementById("image").style.height = (document.getElementById("image").clientWidth - 100) + "px";
     });
     
-    document.querySelector('#btn2').addEventListener('click', () => {
-        document.querySelector('img').src = image2;
-        i = 1;
-    });
-    
-    document.querySelector('#btn3').addEventListener('click', () => {
-        document.querySelector('img').src = image3;
-        i = 2;
-    });
-    
-    document.querySelector('#btn4').addEventListener('click', () => {
-        document.querySelector('img').src = image4;
-        i = 3;
-    });
-    
-    document.querySelector('#btn5').addEventListener('click', () => {
-        document.querySelector('img').src = image5;
-        i = 4;
-    });
+
+    function navigationCheck() {
+        if (i === 0) {
+            previousBtn.style.display = 'none';
+            nextBtn.style.display = 'inline';
+        } else if (i === imageList.length-1) {
+            nextBtn.style.display = 'none';
+            previousBtn.style.display = 'inline';
+        } else {
+            previousBtn.style.display = 'inline';
+            nextBtn.style.display = 'inline';
+        }
+    };
+
+    function initialize() {
+        document.getElementById('image').src = imageList[i];
+        navigationCheck();
+    };
 
 });
